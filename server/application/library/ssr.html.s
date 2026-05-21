54b29738176c2420be68cd34ab3ae4a55e6a2846
111 <111>
2026-05-21 14:51:42 +0900

    fix conflicts

--- server/application/library/ssr.html.s
+++ server/application/library/ssr.html.s
@@ -2,6 +2,13 @@
 <!doctype html>
 <html>
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5SF2QTHZ');</script>
+<!-- End Google Tag Manager -->
 <?js if(__IS_DEV__) { ?>
 <base href='http://127.0.0.1:3000' />
 <?js } ?>
@@ -15,6 +22,10 @@
   <?js } ?>
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SF2QTHZ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
   <div class='app'>$${{ssrHTML}}</div>
   <script type='text/javascript'>
     window.__ssrData__=<?js echo(ssrData) ?>;
