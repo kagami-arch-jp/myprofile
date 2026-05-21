fb648073fdc3fd6143723c1515bb18824dc6cfa2
111 <111>
2026-05-21 15:53:14 +0900

    add gtag

--- server/application/library/ssr.html.s
+++ server/application/library/ssr.html.s
@@ -9,6 +9,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5SF2QTHZ');</script>
 <!-- End Google Tag Manager -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-N3SG49LF6C"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-N3SG49LF6C');
+</script>
 <?js if(__IS_DEV__) { ?>
 <base href='http://127.0.0.1:3000' />
 <?js } ?>
